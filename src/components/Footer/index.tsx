4e9d820da45f1b91878e1a9fe6941da7af490060
weffersson <wefferssonlima.dasilva@gmail.com>
2024-02-27 01:52:56 -0300

    -

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,4 @@
-//!AJUSTES QUE PRECISO  FAZER E "ATUALIZAÇÕES FUTURAS DO CHATBOT":
-  //? OBS: SEPARAR TODOS COMPONENTES DO BOTÃO DE CONTATO E DO ÍCONE DO ROBÔ E DEIXAR EM ARQUIVOS SEPARADOS
-  //? OBS: SEPARAR O HANDLECLICK EM OUTRO COMPONENTE PARA SER USADO NO BOTÃO DE CONTATO 
-  //? OBS: REALIZAR AS MUDANÇAS NA PASTA CORRESPONDENTE DO CHATBOT  
-  //? OBS: FAZER O BACKEND PARA UTILIZAR O CHATBOT (Botpress) 
-
-//-------------------------------------//--------------------------------------------------//
-
+import React, { useRef, useState, forwardRef } from "react";
 import { Container, Flex, Box } from "@/styles/Global";
 import { Text } from "@/styles/Text";
 import { Footer as FooterWrapper } from "./style";
@@ -13,22 +6,9 @@ import { UserImage } from "@/pages/home/style";
 import { userData } from "@/utils/userData";
 import { FaLinkedinIn, FaInstagram, FaWhatsapp } from "react-icons/fa";
 import { Button } from "@/styles/Buttons";
-import { useRef, forwardRef, useState } from "react";
 import { GiRobotAntennas } from "react-icons/gi";
 
-interface RobotIconProps {
-  size: number;
-  style: {
-    color: string;
-    position?: string;
-    bottom?: string;
-    right?: string;
-    zIndex?: number;
-    transition?: string;
-  };
-}
-
-const RobotIcon = forwardRef<HTMLDivElement, RobotIconProps>((props, ref) => {
+const RobotIcon = forwardRef((props, ref) => {
   const [isHovered, setIsHovered] = useState(false);
   const [showMessage, setShowMessage] = useState(false);
 
@@ -52,14 +32,20 @@ const RobotIcon = forwardRef<HTMLDivElement, RobotIconProps>((props, ref) => {
       onClick={handleClick}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      style={{ cursor: "pointer" }}
+      style={{
+        cursor: "pointer",
+        position: "fixed",
+        bottom: "20px",
+        right: "20px",
+        zIndex: 9999,
+        transition: "transform 7.0s ease-out",
+      }}
+      
     >
       <GiRobotAntennas
-        {...props}
-        ref={ref}
+        size={32}
         style={{
-          ...props.style,
-          color: isHovered ? "#FF0000" : props.style.color,
+          color: isHovered ? "#FF0000" : "#808080",
         }}
       />
       {showMessage && (
@@ -89,8 +75,8 @@ const RobotIcon = forwardRef<HTMLDivElement, RobotIconProps>((props, ref) => {
   );
 });
 
-export const Footer = (): JSX.Element => {
-  const chatRef = useRef<HTMLDivElement>(null);
+export const Footer = () => {
+  const chatRef = useRef(null);
 
   return (
     <FooterWrapper id="social-media">
@@ -133,7 +119,7 @@ export const Footer = (): JSX.Element => {
             type="circle"
             as="a"
             target="_blank"
-            href={`https://api.whatsapp.com/send?phone=+55+${userData.whatsappNumber}&text=Ol%C3%A1%2C%20venho%20por%20meio%20do%20seu%20portf%C3%B3lio%20na%20internet%2C%20gostaria%20de%20conhecer%20melhor%20seus%20servi%C3%A7os`}
+            href={`https://api.whatsapp.com/send?phone=+55+${userData.whatsappNumber}&text=Ol%C3%A1%2C%20venho%20por%20meio%20do%20seu%20portf%C3%B3lio%20na%20internet%2C%20gostaria%20de%20conversar%20com%20voc%C3%AA.`}
           >
             <FaWhatsapp />
           </Button>
@@ -147,19 +133,8 @@ export const Footer = (): JSX.Element => {
             <FaLinkedinIn />
           </Button>
         </Flex>
+        <RobotIcon ref={chatRef} />
       </Container>
-      <RobotIcon
-        ref={chatRef}
-        size={32}
-        style={{
-          color: "#808080",
-          position: "fixed",
-          bottom: "20px",
-          right: "20px",
-          zIndex: 9999,
-          transition: "transform 7.0s ease-out",
-        }}
-      />
     </FooterWrapper>
   );
 };
